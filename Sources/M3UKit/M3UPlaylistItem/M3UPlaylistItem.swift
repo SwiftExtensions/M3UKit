b7b86c923c57c039a4000294c7998e39686c8f04
Alexander Algashev <algashev@mail.ru>
2022-02-13 19:16:52 +0300

    Update comments format in M3UPlaylistItem.swift

--- Sources/M3UKit/M3UPlaylistItem/M3UPlaylistItem.swift
+++ Sources/M3UKit/M3UPlaylistItem/M3UPlaylistItem.swift
@@ -7,39 +7,49 @@
 
 import Foundation
 
-/// M3U playlist track info.
+/**
+ The M3U playlist track info.
+ */
 public struct M3UPlaylistItem: Equatable {
-    /// Track runtime in seconds.
-    ///
-    /// Example:
-    /// ```
-    /// #EXTINF:123,Artist Name – Track Title
-    /// ```
-    /// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+    /**
+     The track runtime in seconds.
+    
+     Example:
+     ```
+     #EXTINF:123,Artist Name – Track Title
+     ```
+     More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+     */
     public let runtime: TimeInterval?
-    /// Track title.
-    ///
-    /// Example:
-    /// ```
-    /// #EXTINF:123,Artist Name – Track Title
-    /// ```
-    /// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+    /**
+     The track title.
+    
+     Example:
+     ```
+     #EXTINF:123,Artist Name – Track Title
+     ```
+     More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+     */
     public let title: String
-    /// Group name, if any.
-    ///
-    /// Example:
-    /// ```
-    /// #EXTGRP:Foreign Channels
-    /// ```
-    /// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+    /**
+     A group name, if any.
+    
+     Example:
+     ```
+     #EXTGRP:Foreign Channels
+     ```
+     More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+     */
     public let group: String?
-    /// Track resource.
-    ///
-    /// Example:
-    /// ```
-    /// artist - title.mp3
-    /// ```
-    /// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+    /**
+     The track resource.
+    
+     Example:
+     ```
+     artist - title.mp3
+     ```
+     More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+     */
     public let resource: Resource
     
     
