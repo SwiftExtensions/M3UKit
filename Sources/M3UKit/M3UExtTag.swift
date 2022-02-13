9e712f37ee6177f2cfd63e46bec75c70d0b53956
Alexander Algashev <algashev@mail.ru>
2022-02-13 19:03:19 +0300

    Update comments format in M3UExtTag.swift

--- Sources/M3UKit/M3UExtTag.swift
+++ Sources/M3UKit/M3UExtTag.swift
@@ -5,33 +5,41 @@
 //  Created by Александр Алгашев on 23.01.2022.
 //
 
-/// Extended M3U Tag.
-/// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+/**
+ An extended M3U playlist tag.
+ More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+ */
 public enum M3UExtTag: String {
-    /// File header, must be the first line of the file.
-    ///
-    /// Example:
-    /// ```
-    /// #EXTM3U
-    /// ```
-    /// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+    /**
+     The file header, must be the first line of the file.
+     
+     Example:
+     ```
+     #EXTM3U
+     ```
+     More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+     */
     case extM3U = "#EXTM3U"
-    /// Track information: runtime in seconds and display title of the following resource.
-    ///
-    /// Example:
-    /// ```
-    /// #EXTINF:123,Artist Name – Track Title
-    /// artist - title.mp3
-    /// ```
-    /// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+    /**
+     The track information: runtime in seconds and display title of the following resource.
+     
+     Example:
+     ```
+     #EXTINF:123,Artist Name – Track Title
+     artist - title.mp3
+     ```
+     More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+     */
     case extInf = "#EXTINF:"
-    /// Begin named grouping.
-    ///
-    /// Example:
-    /// ```
-    /// #EXTGRP:Foreign Channels
-    /// ```
-    /// More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+    /**
+     The begin named grouping.
+     
+     Example:
+     ```
+     #EXTGRP:Foreign Channels
+     ```
+     More info see [M3U](https://en.wikipedia.org/wiki/M3U).
+     */
     case extGrp = "#EXTGRP:"
     
     
