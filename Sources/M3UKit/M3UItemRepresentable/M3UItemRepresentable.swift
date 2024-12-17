95e08f973cff666d03ea025d6f593dbf6f098329
Alexander Algashev <algashev@mail.ru>
2024-12-17 19:45:26 +0300

    Add item to M3UItemRepresentable.swift

--- Sources/M3UKit/M3UItemRepresentable/M3UItemRepresentable.swift
+++ Sources/M3UKit/M3UItemRepresentable/M3UItemRepresentable.swift
@@ -9,3 +9,14 @@
  Protocol for [M3U](https://en.wikipedia.org/wiki/M3U) items.
  */
 public protocol M3UItemRepresentable { }
+
+public extension M3UItemRepresentable {
+    /**
+     The [M3U](https://en.wikipedia.org/wiki/M3U) playlist track info.
+     */
+    var item: M3UItem? {
+        self as? M3UItem
+    }
+    
+    
+}
