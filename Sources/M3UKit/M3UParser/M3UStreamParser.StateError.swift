875c8e296e505e812d3496f7d61b73bc4fab68ec
Alexander Algashev <algashev@mail.ru>
2025-12-12 20:47:00 +0300

    Add M3UStreamParser.StateError.swift

---
+++ Sources/M3UKit/M3UParser/M3UStreamParser.StateError.swift
@@ -0,0 +1,49 @@
+//
+//  M3UStreamParser.StateError.swift
+//  M3UKit
+//
+//  Created by Александр Алгашев on 12/12/2025.
+//
+
+import Foundation
+
+public extension M3UStreamParser {
+    /**
+     Stream parser state errors.
+     */
+    enum StateError: Error, Equatable {
+        /**
+         `feed(_:)` has not been called.
+         */
+        case feedNotCalled
+        /**
+         `finish()` has not been called.
+         */
+        case finishNotCalled
+    }
+    
+    
+}
+
+// MARK: - LocalizedError
+
+extension M3UStreamParser.StateError: LocalizedError {
+    /**
+     A localized description of the stream parser state error.
+     */
+    public var errorDescription: String? {
+        let description: String
+        switch self {
+        case .feedNotCalled:
+            let message = #"feed(_:) has not been called."#
+            description = NSLocalizedString(message, comment: message)
+        case .finishNotCalled:
+            let message = #"finish() has not been called."#
+            description = NSLocalizedString(message, comment: message)
+        }
+
+        return description
+    }
+    
+    
+}
